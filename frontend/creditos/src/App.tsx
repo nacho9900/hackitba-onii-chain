ea8d75912a5cacf29beef6d794a6aff2929eb35c
Nicolás Marcantonio <marcantonionicolas@gmail.com>
2022-04-02 16:07:25 -0300

    added bootstrap

--- frontend/creditos/src/App.tsx
+++ frontend/creditos/src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { Alert } from "react-bootstrap";
+import "./App.scss";
 import logo from "./logo.svg";
-import "./App.css";
+
 
 function App() {
   return (
@@ -10,14 +12,19 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Alert variant="success">
+          <Alert.Heading>Hey, nice to see you</Alert.Heading>
+          <p>
+            Aww yeah, you successfully read this important alert message. This example
+            text is going to run a bit longer so that you can see how spacing within an
+            alert works with this kind of content.
+          </p>
+          <hr />
+          <p className="mb-0">
+            Whenever you need to, be sure to use margin utilities to keep things nice
+            and tidy.
+          </p>
+        </Alert>
       </header>
     </div>
   );
