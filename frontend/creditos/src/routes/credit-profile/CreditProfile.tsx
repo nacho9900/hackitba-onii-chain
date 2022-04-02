696d2920dd4e80c3a0cc992db4cd38f36a6f44c7
Nacho <nachonegrocaino@gmail.com>
2022-04-02 18:49:25 -0300

    add: CreditProfile

---
+++ frontend/creditos/src/routes/credit-profile/CreditProfile.tsx
@@ -0,0 +1,105 @@
+import { FC, useMemo, useState } from "react";
+import { Profile, Project, ProjectType } from "../../types";
+import { Col, Container, ProgressBar, Row, Table } from "react-bootstrap";
+import "./credit-profile.scss";
+
+const PROFILE: Profile = {
+  name: "Leonel Messi",
+  bio: "Jugador de futbol 🇦🇷 amateur en Colegiales. Papa de 3 hijos y un perro.",
+  imageUrl:
+    "https://img.a.transfermarkt.technology/portrait/big/28003-1631171950.jpg?lm=1",
+  scoring: 7,
+  location: "Paris, Francia 🇫🇷",
+};
+
+const PROJECT_1: Project = {
+  name: "Champions 2007 🏆",
+  createdOn: new Date("2007-11-02"),
+  startOn: new Date("2007-11-02"),
+  category: "Deportes",
+  description: "",
+  invested: 100000,
+  earning: 2540000,
+  type: ProjectType.Service,
+};
+
+const PROJECT_2: Project = {
+  name: "Lote de Pelotas Jabulani Mundial 2010 ⚽️",
+  createdOn: new Date("2010-05-02"),
+  startOn: new Date("2010-06-25"),
+  category: "Deportes",
+  description: "",
+  invested: 1000000,
+  earning: 500000,
+  type: ProjectType.Service,
+  quantity: 500,
+};
+
+const PROJECTS = [PROJECT_1, PROJECT_2];
+
+export const CreditProfile: FC<{}> = () => {
+  const [profile, setProfile] = useState<Profile>(PROFILE);
+  const [projects, setProjects] = useState<Project[]>(PROJECTS);
+  const progress = useMemo(() => 10 * profile.scoring, [profile.scoring]);
+
+  return (
+    <>
+      <Container className="profile-container" fluid>
+        <Row>
+          <Col xs={12} className="d-flex justify-content-center pt-2">
+            <img
+              src={profile.imageUrl}
+              className="image_container"
+              alt="Profile Image"
+            />
+          </Col>
+          <Col xs={12} className="d-flex justify-content-center pt-2">
+            <h1>{profile.name}</h1>
+          </Col>
+          <Col xs={12} className="d-flex justify-content-center pt-2">
+            <p>{profile.bio}</p>
+          </Col>
+          <Col xs={12} className="d-flex justify-content-center pt-2">
+            <p>Puntaje Crediticio: {profile.scoring}</p>
+          </Col>
+        </Row>
+        <Row className="d-flex justify-content-center">
+          <Col xs={4}>
+            <ProgressBar variant="success" now={progress} />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} className="px-5">
+            <h1>Proyectos</h1>
+          </Col>
+          <Col xs={12} className="px-5">
+            <Table striped bordered>
+              <thead>
+                <th>Nombre</th>
+                <th>Creado En</th>
+                <th>Comenzo En</th>
+                <th># Productos</th>
+                <th>Inversion</th>
+                <th>Ganancia</th>
+              </thead>
+              <tbody>
+                {projects.map((project) => (
+                  <tr>
+                    <td>{project.name}</td>
+                    <td>{project.createdOn.toISOString()}</td>
+                    <td>{project.startOn.toISOString()}</td>
+                    <td>
+                      {project.quantity !== undefined ? project.quantity : "-"}
+                    </td>
+                    <td className="text-end">${project.invested}</td>
+                    <td className="text-end">${project.earning}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
