24183f02670a633ab6f2d990f9cb3c71df7ca77a
Nacho <nachonegrocaino@gmail.com>
2022-04-03 02:10:48 -0300

    style: crypto-transfers

--- frontend/creditos/src/routes/CryptoTransfer/CryptoTransfer.tsx
+++ frontend/creditos/src/routes/CryptoTransfer/CryptoTransfer.tsx
@@ -1,72 +1,81 @@
 import "./CryptoTransfer.scss";
 import logo from "./img/logo.svg";
-import { Col, Container, Row, Table } from "react-bootstrap";
+import { Button, Col, Container, Row, Table } from "react-bootstrap";
 import { useMetaMask } from "metamask-react";
 
 export default function CryptoTransfer() {
+  const { status, connect, account, chainId, ethereum } = useMetaMask();
 
-    const { status, connect, account, chainId, ethereum } = useMetaMask();
-
-    const getMetamaskMsg = (status: string) => {
-        switch (status) {
-            case "initializing":
-                return (<div className="address">Synchronisation with MetaMask ongoing...</div>);
-            case "unavailable":
-                return (<div className="address">MetaMask not available :(</div>);
-            case "notConnected":
-                return (<button className="button" onClick={connect}>Connect to MetaMask</button>);
-            case "connecting":
-                return (<div className="address">Connecting...</div>);
-            case "connected":
-                return (<div className="address">Connected account {account} on chain ID {chainId}</div>);
-            default:
-                return (<div>default(</div>);
-        }
+  const getMetamaskMsg = (status: string) => {
+    switch (status) {
+      case "initializing":
+        return <div className="address">Sincronizando con MetaMask...</div>;
+      case "unavailable":
+        return <div className="address">MetaMask no disponible :(</div>;
+      case "notConnected":
+        return (
+          <Button className="button" onClick={connect}>
+            Conectar a MetaMask
+          </Button>
+        );
+      case "connecting":
+        return (
+          <Button className="button" onClick={connect} disabled>
+            Conectando...
+          </Button>
+        );
+      case "connected":
+        return (
+          <Button className="address connected" disabled>
+            Ya conectado
+          </Button>
+        );
+      default:
+        return <div>default(</div>;
     }
+  };
 
-    return (
-
-        <Container className="justify-content-center pt-5 px-10">
-            <Row className="projects-container justify-content-center">
-                <Col>
-                </Col>
-                <Col className="justify-content-center">
-                    <h1>Retirar</h1>
-                </Col>
+  return (
+    <Container className="justify-content-center pt-5 px-10">
+      <Row className="d-flex justify-content-center">
+        <Col xs={6}>
+          <h1 className="text-white">Retirar</h1>
+        </Col>
+      </Row>
+      <Row className="d-flex justify-content-center">
+        <Col xs={6}>
+          <Container className="crypto-transfer-card" fluid>
+            <Row className="justify-content-center">
+              <Col xs={12}>
+                <div className="d-flex d-inline justify-content-center">
+                  <img width={50} height={50} src={logo} />
+                  {getMetamaskMsg(status)}
+                </div>
+              </Col>
             </Row>
-            <Row className="projects-container justify-content-center">
-                <Col className="py-1 justify-content-center">
-                    <div className="d-flex d-inline">
-                        <img width={50} height={50} src={logo} />
-                        {getMetamaskMsg(status)}
-                        <form>
-                            <h3>Ingresar</h3>
-                            <div className="form-group">
-                                <label>Monto a retirar</label>
-                                <input
-                                    type="number"
-                                    className="form-control"
-                                    placeholder="monto"
-                                    name="monto"
-                                />
-                            </div>
-                            <div>
-                                <button
-                                    type="submit"
-                                    className="btn btn-primary btn-block">
-                                    Procesar
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </Col>
+            <Row>
+              <Col xs={12}>
+                <form>
+                  <div className="form-group">
+                    <label>Monto a retirar</label>
+                    <input
+                      type="number"
+                      className="form-control"
+                      placeholder="monto"
+                      name="monto"
+                    />
+                  </div>
+                  <div className="mt-2 d-flex justify-content-end">
+                    <Button type="submit" variant="primary">
+                      Procesar
+                    </Button>
+                  </div>
+                </form>
+              </Col>
             </Row>
-        </Container>
-
-
-    );
-
-
-
-
+          </Container>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
